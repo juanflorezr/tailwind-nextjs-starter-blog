27acdc9f6ac352977e9e3d1451f1f02f687dbe25
Juan José Flórez Restrepo <juan.florezr@gmail.com>
2024-02-04 06:03:31 -0500

    Update siteMetadata.js
    Update Title

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'PTMNT',
   author: 'Tails Azimuth',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
